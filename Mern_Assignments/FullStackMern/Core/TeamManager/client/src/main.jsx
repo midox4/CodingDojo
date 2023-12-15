12e61498ef34a5d40e64b6a024d10244cd99e4dc
Hafsi Ahmed <crasypo@gmail.com>
2023-12-15 12:01:57 +0100

    adding teamManager folder

---
+++ Mern_Assignments/FullStackMern/Core/TeamManager/client/src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter as Rout } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Rout>
+    <App />
+
+  </Rout>
+)
